3e5eafcc45158bddef6152acf0c48d124825ea53
sameh20 <samehaboelmagd49@gmail.com>
2021-06-11 02:56:10 +0200

    Updates

---
+++ precache-manifest.b5b00e2747498ecc1cf6d480956a8f6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392205da452dda68d5a7d1ba4405027c",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "31e8cd142eeb957827f3",
+    "url": "/weather-app/static/css/main.dbe25460.chunk.css"
+  },
+  {
+    "revision": "21a79572b884fde6d694",
+    "url": "/weather-app/static/js/2.5a727e64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.5a727e64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31e8cd142eeb957827f3",
+    "url": "/weather-app/static/js/main.1e8b8cd7.chunk.js"
+  },
+  {
+    "revision": "bdcb51b63d9249268df7",
+    "url": "/weather-app/static/js/runtime-main.f1c906e7.js"
+  }
+]);
